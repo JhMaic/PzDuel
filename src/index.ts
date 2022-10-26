f176ce0e3c2b20588da6a985299ab7d2f7c8d26b
jinhongChen <owcin.cjh@gmail.com>
2022-10-26 23:34:28 +0900

    rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,25 @@
-import {Application, Container, Sprite, Texture} from "pixi.js";
+import * as PIXI from "pixi.js";
 import image from "@assets/clampy.png";
 
-const app = new Application({
-    width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
-});
+const app = new PIXI.Application({backgroundColor: 0x1099bb});
 document.body.appendChild(app.view);
 
-const container = new Container();
+// create a new Sprite from an image path
+const bunny = PIXI.Sprite.from(image);
 
-app.stage.addChild(container);
+// center the sprite's anchor point
+bunny.anchor.set(0.5);
 
-// Create a new texture
-const texture = Texture.from(image);
+// move the sprite to the center of the screen
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-    const bunny = new Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
-}
-
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+app.stage.addChild(bunny);
 
 // Listen for animate update
 app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent transformation
+    bunny.rotation += 0.1 * delta;
 });
---
+++ src/scripts/core/Panzer.ts
@@ -0,0 +1,9 @@
+import type {Camp, Commander} from "@/scripts/core/types";
+
+export class Panzer implements Commander {
+
+    constructor(commander: Commander,
+                camp: Camp) {
+    }
+
+}
