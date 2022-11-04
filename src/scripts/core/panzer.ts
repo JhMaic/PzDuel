dd67caed1f44d69bbcde724b10313aa6452c7b55
jinhongChen <owcin.cjh@gmail.com>
2022-11-04 12:23:53 +0900

    feature commit

--- src/scripts/core/panzer.ts
+++ src/scripts/core/panzer.ts
@@ -4,6 +4,8 @@ export class Panzer implements Commander {
 
     constructor(commander: Commander,
                 camp: CAMP) {
+        let feature = ' feaature ';
+        let feature 2 = 'feature2';
     }
 
 }
